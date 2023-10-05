26efd64ed55dff97374bc199d4533b150a2643c8
atheist31 <rkb080@gmail.com>
2023-10-05 22:21:36 +0530

    done

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      {/* <Banner /> */}
+      {/* <Card /> */}
+      {/* <Testimonial />
+      <Blog />
+      <Footer /> */}
+    </>
+  );
+}
